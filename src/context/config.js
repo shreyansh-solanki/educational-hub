f449fe3c27043f2468e311f3fb660037fd161468
shreyansh <shreyansh1029@gmail.com>
2021-11-20 23:06:01 +0530

    created config file

---
+++ src/context/config.js
@@ -0,0 +1,8 @@
+const API_BASE_URL = 'https://www.googleapis.com/youtube/v3';
+
+export async function apiGet(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
+  return response;
+}
