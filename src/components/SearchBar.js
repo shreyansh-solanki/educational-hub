a6b4fd7c6ed58ba9f4becd0a846545e97865a11a
shreyansh <shreyansh1029@gmail.com>
2021-11-27 13:40:39 +0530

    make search bar working with input results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,4 @@
-import {
-  faLessThan,
-  faMicrophone,
-  faSearch,
-} from '@fortawesome/free-solid-svg-icons';
+import { faMicrophone, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import { Input, InputGroup, Nav } from 'rsuite';
@@ -20,11 +16,12 @@ const SearchContent = () => {
 
   // const onInptChange = ev => {
   //   setInput(ev);
+  //   // console.log(ev);
   // };
 
   // const [results, setResults] = useState(null);
   // const maxResult = 1;
-  // let i = 0;
+
   // const onSearch = () => {
   //   fetch(
   //     `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResult}&q=${input}&type=video&key=AIzaSyBI0F6ZXwj2WNw9g57Y7vkZmhxE1F3r80k`
@@ -32,19 +29,12 @@ const SearchContent = () => {
   //     .then(r => r.json())
   //     .then(result => {
   //       setResults(result);
-  //       console.log(result.items[i++].snippet.channelTitle);
+  //       console.log(result);
   //     });
   // };
-  // // let count = 0;
-  // // const allValue = () => {
-  // //   for (let i = 0; i <= maxResult; i++) {
-  // //     console.log(count++);
-  // //   }
-  // //   count = 0;
-  // // };
+
   // const onKeyDown = ev => {
   //   if (ev.keyCode === 13) {
-  //     // allValue();
   //     onSearch();
   //   }
   // };
@@ -56,11 +46,8 @@ const SearchContent = () => {
   //   if (results && results.length > 0) {
   //     return (
   //       <div>
-  //         {results.map(item => (
-  //           <div>
-  //             {/* key={item.items[allValue()].id.videoId} */}
-  //             {item.items[0].snippet.channelTitle}
-  //           </div>
+  //         {results.items.map(item => (
+  //           <div key={item.id.videoId}>{item.snippet.channelTitle}</div>
   //         ))}
   //       </div>
   //     );
@@ -80,8 +67,10 @@ const SearchContent = () => {
               style={inputStyles}
               placeholder="Search"
             />
+            {/* {console.log(renderResults())} */}
+            {/* {renderResults()} */}
             <InputGroup.Button
-              // onClick={onSearch}
+              //  onClick={onSearch}
               style={{ marginLeft: 260 }}
             >
               <FontAwesomeIcon icon={faSearch} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Input, InputGroup } from 'rsuite';
+import { apiGet } from '../context/config';
 import NavBar from '../components/NavBar/NavBar';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const onSearch = () => {
+    apiGet(`/search?part=snippet&maxResults=1&q=${input}&`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInptChange = ev => {
+    setInput(ev);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (input.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && input.length > 0) {
+      // if (results.items.length) {
+      // <div>
+      return results.items.map(item => (
+        <div key={item.id.videoId}>{item.snippet.channelTitle}</div>
+      ));
+      // {results.items.map(item => (
+      // <div key={item.id.videoId}>{item.snippet.channelTitle}</div>
+      // ))}
+      // </div>;
+    }
+    // }
+    return null;
+  };
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
+      {/* <SearchBar /> */}
+      {/* <InputGroup inside style={{ width: 100, height: 30 }}> */}
+      <Input
+        onChange={onInptChange}
+        onKeyDown={onKeyDown}
+        value={input}
+        size="md"
+        // style={inputStyles}
+        placeholder="Search"
+      />
+      {/* </InputGroup> */}
+
+      <div>{renderResults()}</div>
     </>
   );
 };
